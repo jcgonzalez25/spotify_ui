741df8f28ea8c0a53e3e2d4b1dc01c35edfc7a80
JC Gonzalez <devgonzalez25@gmail.com>
2018-12-07 23:23:56 -0500

    Added color relative to spotify, and some boiler plate scripts for music control

---
+++ current_song_script.js
@@ -0,0 +1,25 @@
+let SongInfo = {
+  isPlaying:function(){
+
+  }
+  getSong:function(){
+
+  }
+  getAlbumArt():function(){
+
+  }
+  showPlayingStatus():function(){
+
+  }
+  hidePlayingStatus():function(){
+
+  }
+}
+let SongController = {
+  playSong:function(){
+
+  }
+  pauseSong:function(){
+
+  }
+}
