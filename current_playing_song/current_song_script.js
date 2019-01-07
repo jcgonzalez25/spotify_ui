2f67c7c0f3f5735e5008e2dd0ebd8f82e84c99fb
JC Gonzalez <devgonzalez25@gmail.com>
2019-01-07 02:55:51 -0600

    A class to make the animation controls easier. Animation changed based off the fullScreen property of CAnimation.

--- current_playing_song/current_song_script.js
+++ current_playing_song/current_song_script.js
@@ -6,16 +6,59 @@ const song_progress_bar                  = document.querySelector(".progress");
 const song_info_div                      = document.querySelector(".song_info_div");
 const screenHeight                       = window.innerHeight;
 const navigation_original_height         = document.querySelector(".navigation").offsetHeight;
-let SongInfo, SongController, SongState;
+let SongInfo, SongController, SongState,CurrentSongAnimation;
+
 (function() {
   song_statusbar_element.addEventListener("click", () => SongInfo.showPlayingStatus());
   document.querySelector("#now_playing_toggle_button").addEventListener("click",()=> SongInfo.showPlayingStatus());
 })();
-
-SongState = {
-  onFullScreen: false,
-  isPlaying: false
+let CAnimation = {
+  fullScreen:null,
+  NavigationElement:document.querySelector(".navigation"),
+  init:function(){
+    if(!this.fullScreen)
+      this.fullScreenSetup();
+  },
+  fullScreenSetup:function(){
+    song_info_div.style.height          = "0px";
+    this.NavigationElement.style.height = this.NavigationElement.clientHeight + "px";
+    song_info_div.classList.remove("d-none");
+  },
+  animateNav:function(){
+    let navigation_element_height_value;
+    let id = setInterval(() => {
+      navigation_element_height_value = getIntValue(this.NavigationElement.style.height);
+      if(this.fullScreen){
+        this.NavigationElement.style.height = navigation_element_height_value + 1 + "px";
+        if(navigation_element_height_value === navigation_original_height)
+          clearInterval(id);
+      }else{
+        this.NavigationElement.style.height = navigation_element_height_value - 1 + "px";
+        if(navigation_element_height_value === 0)
+          clearInterval(id);
+      }
+    }, 10);
+  },
+  animateMainContent:function(){
+    let topAnimation = setInterval(() => {
+      song_info_height_value = getIntValue(song_info_div.style.height);
+      if (this.fullScreen) {
+        song_info_div.style.height = song_info_height_value - 30 + "px";
+        if(song_info_height_value === 0)
+          clearInterval(topAnimation)
+          song_info_div.classList.add("d-none");
+      } else {
+        song_info_div.classList.remove("d-none");
+        song_info_div.style.height = song_info_height_value + 30 + "px";
+        if (song_info_height_value >= screenHeight - 20) {
+          song_info_div.style.height = (screenHeight - song_info_height_value) + song_info_height_value + "px";
+          clearInterval(topAnimation);
+        }
+      }
+    }, 10);
+  }
 };
+
 SongInfo = {
   getSong: function() {},
   getAlbumArt: function() {},
@@ -23,52 +66,16 @@ SongInfo = {
     let fullScreen         = !song_info_div.classList.contains("d-none");
     let navigation_element = document.querySelector(".navigation");
     let song_info_height_value;
-    if (!fullScreen) {
-      song_info_div.style.height      = "0px";
-      navigation_element.style.height = navigation_element.clientHeight + "px";
-      song_info_div.classList.remove("d-none");
-    }
-    let animateNav = ()=>{
-      let navigation_element_height_value;
-      let id = setInterval(() => {
-        navigation_element_height_value = getIntValue(navigation_element.style.height);
-        if(fullScreen){
-          navigation_element.style.height = navigation_element_height_value + 1 + "px";
-          if(navigation_element_height_value === navigation_original_height)
-            clearInterval(id);
-        }else{
-          navigation_element.style.height = navigation_element_height_value - 1 + "px";
-          if(navigation_element_height_value === 0)
-            clearInterval(id);
-        }
-      }, 10);
-    }
-    let animateMainContent =  ()=>{
-      let topAnimation = setInterval(() => {
-        song_info_height_value = getIntValue(song_info_div.style.height);
-        if (fullScreen) {
-          song_info_div.style.height = song_info_height_value - 30 + "px";
-          if(song_info_height_value === 0)
-            clearInterval(topAnimation)
-            song_info_div.classList.add("d-none");
-        } else {
-          song_info_div.classList.remove("d-none");
-          song_info_div.style.height = song_info_height_value + 30 + "px";
-          if (song_info_height_value >= screenHeight - 20) {
-            song_info_div.style.height = (screenHeight - song_info_height_value) + song_info_height_value + "px";
-            clearInterval(topAnimation);
-          }
-        }
-      }, 10);
-    }
-    animateNav();
-    animateMainContent();
+    CAnimation.fullScreen  = !song_info_div.classList.contains("d-none");
+    CAnimation.init();
+    CAnimation.animateNav();
+    CAnimation.animateMainContent();
   },
   hidePlayingStatus: function() {
 
   }
-}
+};
 SongController = {
   playSong: function() {},
   pauseSong: function() {}
-}
+};
