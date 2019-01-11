b26bff30d5e38c5c9cdfe00a3b7a9d37c6d7a598
JC Gonzalez <devgonzalez25@gmail.com>
2019-01-11 00:03:28 -0600

    using scroll on overflow to handle album control feature

--- current_playing_song/album_art_carousel/main.js
+++ current_playing_song/album_art_carousel/main.js
@@ -1,5 +1,31 @@
+//figure album movement
+//testing for show playing status
+(function(){
+  SongInfo.showPlayingStatus();
+})();
 let Carousel = {
-  swiperOn:()=>{
-    console.log("should enable sliding on images");
+  AlbumWrapper:null,
+  centeredScroll:328,
+  moveArtwork:function(event){
+    console.log("hello");
+  },
+  focusOnMainElement:function(){
+    this.AlbumWrapper.scrollLeft = this.centeredScroll;
+  },
+  swiperOn:function(){
+    this.AlbumWrapper = document.querySelector("#album_art_Wrapper");
+    this.AlbumWrapper.addEventListener("scroll",event=>this.moveArtwork(event));
+    this.AlbumWrapper.addEventListener("touchend",event=>this.dragoff(event));
+    this.AlbumWrapper.addEventListener("touchstart",event=>this.dragon(event));
+  },
+  dragoff:function(event){
+    let onNextSong = false;
+    console.log("dragging done");
+    this.AlbumWrapper.style.overflow= "hidden";
+    this.AlbumWrapper.scrollLeft = this.centeredScroll;
+  },
+  dragon:function(event){
+    this.AlbumWrapper.style.overflow = "scroll";
+    console.log("this.Wrapper");
   }
 }
