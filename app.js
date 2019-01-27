72c2ba41ea1a2779479f30486ab37175baa948eb
JC Gonzalez <devgonzalez25@gmail.com>
2019-01-27 05:51:59 -0500

    updating file structure for the web application

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('path');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+
+
+mongoose.promise = global.Promise;
+
+
+
+const isProduction = process.env.NODE_ENV === 'production'
+
 const app = express();
+app.use(cors());
+//enabling cross origin resourse
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public'));
+app.use(session(
+    {secret:'passport-tutorial',cookie:{maxAge:600},resave:false,saveUninitialized:}
+));
+
+
+
 const port = 3000;
-
-
-
-app.use('/',express.static(__dirname))
+const Query = require('./query.js');
+const Validator = require('./Validator.js');
+app.use('/',express.static(__dirname));
 app.listen(port, ()=>console.log(`Example app listening on port ${port}`));
--- current_playing_song/album_art_carousel/songs.js
+++
@@ -1,4 +0,0 @@
-fetch('http://localhost:3000/song_feeder')
-.then(function(response){
-    console.log(response);
-})
