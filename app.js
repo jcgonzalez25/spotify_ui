7fd976a4ddbb07edf6ea85148fc7b3b527b0248f
JC Gonzalez <devgonzalez25@gmail.com>
2019-01-27 06:16:27 -0500

    changes to file structure

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('path');
-const session = require('express-session');
-const cors = require('cors');
-const mongoose = require('mongoose');
 const errorHandler = require('errorhandler');
-
-
+const Query = require('./query.js');
+const Validator = require('./Validator.js');
 mongoose.promise = global.Promise;
 
 
 
-const isProduction = process.env.NODE_ENV === 'production'
-
+const isProduction = process.env.NODE_ENV === 'production';
 const app = express();
 app.use(cors());
 //enabling cross origin resourse
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'public'));
-app.use(session(
-    {secret:'passport-tutorial',cookie:{maxAge:600},resave:false,saveUninitialized:}
-));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 
 const port = 3000;
-const Query = require('./query.js');
-const Validator = require('./Validator.js');
+
 app.use('/',express.static(__dirname));
 app.listen(port, ()=>console.log(`Example app listening on port ${port}`));
